72cc0534b5b7d54c60bc9e61e4acdcc9380a3b72
SAITO Tomomi <t-saito@project-flora.net>
2015-12-15 01:01:16 +0900

    [swift] mod

--- swift/megane_golf.swift
+++ swift/megane_golf.swift
@@ -1,12 +1,9 @@
 func p(t:Int)->[[String]]{var a=[[String]]()
-for(var i=0;i<t;i++){a.append(readLine()!.characters.split{$0==" "}.map(String.init))}
+for _ in 0..<t{a.append(readLine()!.characters.split{$0==" "}.map(String.init))}
 return a}
-let s=Int(readLine()!)!
-let m=p(s)
-let t=Int(readLine()!)!
-let n=p(t)
-var f=0
-for(var h=0;h<=s-t && f==0;h++){for(var i=0;i<=s-t && f==0;i++){var e=1
-for(var j=0;j<t && e>0;j++){for(var k=0;k<t && e>0;k++){if(m[h+j][i+k] != n[j][k]){e=0}}}
+let s=Int(readLine()!)!,m=p(s),t=Int(readLine()!)!,n=p(t)
+d:for h in 0...s-t{for i in 0...s-t{var e=1
+c:for j in 0..<t{for k in 0..<t{if(m[h+j][i+k] != n[j][k]){e=0
+continue c}}}
 if(e>0){print(h,i)
-f=1}}}
+break d}}}
