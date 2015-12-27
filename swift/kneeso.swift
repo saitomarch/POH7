6abc53fb54bdd981f0fb1f941c0ad4afac2a83e0
SAITO Tomomi <saitomarch@apost.plala.or.jp>
2015-12-27 14:37:24 +0900

    Update kneeso.swift
    カラーパターンの修正を容易に

--- swift/kneeso.swift
+++ swift/kneeso.swift
@@ -1,9 +1,9 @@
 let width = Int(readLine()!)!
 let length = Int(readLine()!)!
+let pattern = ["R", "W"];
 var str = ""
 for (var idx = 0; idx < length; idx++) {
-    let colorStr = (idx / width) % 2 == 0 ? "R" : "W"
+    let colorStr = pattern[(idx / width) % pattern.count]
     str += colorStr
 }
 print(str)
-
