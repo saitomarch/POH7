4a9de186b9df910402805a3a988492e263593874
SAITO Tomomi <saitomarch@apost.plala.or.jp>
2015-12-27 10:47:25 +0900

    Update cardigan.cpp
    記述を簡潔に

--- cpp/cardigan.cpp
+++ cpp/cardigan.cpp
@@ -5,11 +5,10 @@ int main(void){
     string str;
     getline(cin, str);
     int times = stoi(str);
-    int num = times;
-    for (int cnt = 1; cnt < times; cnt++) {
-        num *= times - cnt;
+    int num = 1;
+    for (int cnt = 1; cnt <= times; cnt++) {
+        num *= cnt;
     }
     cout << num << endl;
     return EXIT_SUCCESS;
 }
-
